451242aaee4f0540b872b4f6c0ea2f1e81745a17
miralvyas <miral.vyas.corp@gmail.com>
2024-05-13 16:13:48 +0100

    Tailwind CSS

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -11,7 +11,7 @@ const users:User[] = await res.json()
 
  return (
     <>
-      <h1>Users Details</h1>
+      <h1 className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>Users Details</h1>
       <ul>
       {users.map(user=>{
        return <li key={user.id}>{user.id} : {user.name}</li>
