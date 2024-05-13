2706d1d9c04edf30f7aefb12574dec0ab9216790
miralvyas <miral.vyas.corp@gmail.com>
2024-05-13 14:55:59 +0100

    Demonstrated usage of Client Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello</h1>
       <Link href="/users/newuser"> New User</Link>
+      <ProductCard />
     </main>
   )
 }
