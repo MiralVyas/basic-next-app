f52e27ebe040364086b51fa753aabb2a21022c24
miralvyas <miral.vyas.corp@gmail.com>
2024-05-13 15:29:03 +0100

    Fetching data and usage of typescript

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-const UsersPage = () => {
-  return (
-    <div>
-      Users Page
-    </div>
+interface User {
+  id:string,
+  name:string
+}
+
+const UsersPage = async () => {
+const res = await fetch('https://jsonplaceholder.typicode.com/users');
+const users:User[] = await res.json()
+
+ return (
+    <>
+      <h1>Users Details</h1>
+      <ul>
+      {users.map(user=>{
+       return <li key={user.id}>{user.id} : {user.name}</li>
+      })}
+      </ul>
+    </>
   )
 }
 
