765b451ec48bb8314ff79f928a02d664eb47feda
miralvyas <miral.vyas.corp@gmail.com>
2024-05-13 16:48:40 +0100

    Updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,13 +19,7 @@ const config: Config = {
     require('daisyui'),
   ],
   daisyui: {
-    darkTheme: false, // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: ["light"],
   },  
 }
 export default config
